acfa15948a68803bc8bf25c4c1dedb1d64d39905
Y. Z. Chen <754097987@qq.com>
2022-03-15 23:58:07 +0800

    [Update] local storage is not shared.
    Took 15 minutes

--- popup.js
+++ popup.js
@@ -1,11 +1,11 @@
 function onload() {
     console.log("abc", localStorage.getItem("github-nickname-source"));
-    document.getElementById("content").innerText = localStorage.getItem("github-nickname-source");
+    document.getElementById("content").value = localStorage.getItem("github-nickname-source");
 }
 function onsave() {
     console.log("save", document.getElementById("content").innerText);
     console.log("save0", document.getElementById("content"));
-    localStorage.setItem("github-nickname-source", document.getElementById("content").innerText);
+    localStorage.setItem("github-nickname-source", document.getElementById("content").value);
 }
 window.onload = onload;
 document.getElementById("save-button").addEventListener("click", onsave);
